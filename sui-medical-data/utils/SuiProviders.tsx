e2b03ea448339017d28ded49ea41b4f47136f51d
fOmar24 <savage0764@gmail.com>
2025-03-07 17:59:02 +0300

    Update SuiProviders.tsx
    added queryclient

--- sui-medical-data/utils/SuiProviders.tsx
+++ sui-medical-data/utils/SuiProviders.tsx
@@ -1,9 +1,11 @@
+"use client";
+
 import React from 'react';
 import { SuiClientProvider, WalletProvider } from '@mysten/dapp-kit';
 import { getFullnodeUrl } from '@mysten/sui/client';
 import { Toaster } from 'sonner';
 import MedicalForm from '../components/MedicalForm';
-
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const networks = {
   testnet: { url: getFullnodeUrl('testnet') },
@@ -15,18 +17,25 @@ interface SuiProvidersProps {
   network?: 'testnet' | 'mainnet';
 }
 
+// Create a QueryClient instance
+const queryClient = new QueryClient();
+
 export const SuiProviders: React.FC<SuiProvidersProps> = ({ 
   children, 
   network = 'testnet' 
 }) => {
   return (
-    <SuiClientProvider networks={networks} defaultNetwork={network}>
-      <WalletProvider>
-        {children}
-        <MedicalForm packageId="0x9a37905ef941cd7f0270f0b363c7d202dd5d105102a590051895589f3959431c" moduleName="medicalresearchdata" />
-        <Toaster richColors position="top-right" />
-    
-      </WalletProvider>
-    </SuiClientProvider>
+    <QueryClientProvider client={queryClient}>
+      <SuiClientProvider networks={networks} defaultNetwork={network}>
+        <WalletProvider>
+          {children}
+          <MedicalForm 
+            packageId="0x9a37905ef941cd7f0270f0b363c7d202dd5d105102a590051895589f3959431c" 
+            moduleName="medicalresearchdata" 
+          />
+          <Toaster richColors position="top-right" />
+        </WalletProvider>
+      </SuiClientProvider>
+    </QueryClientProvider>
   );
 };
